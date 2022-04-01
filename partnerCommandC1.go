14123ec7dac2362bd1f33aeeebae3c6eac741d50
mangowhite <75037449+mangowhite@users.noreply.github.com>
2022-04-01 13:15:59 -0300

    Started first text command of Entrance System
    Comments were added to the command, production will start soon...

---
+++ partnerCommandC1.go
@@ -0,0 +1,7 @@
+{{/*
+MADE BY: mangowhite#0001 (209208777365389312)
+REQUESTED BY: 𝑴𝒆𝒍𝒌 𝑻𝒆𝒓𝒕𝒋𝒊𝒆♡☘#0001 (441642976058277898)
+DESCRIPTION: This is a command that takes many arguments and is used to decide wether or not two elements want to "partner" up, as requested by the buyer. Included in this file are the following operations: 1. The first user locks himself and the target into a database value and leaves the second subject to choose if they'll be connected to each other; 2. The second user uses the options <accept|reject> to choose if they'll be connected or not to the request sender; 3. Finally, if no args are exemplified, the output will be the current partner of the supplied user trigger.
+RECOMMENDED TRIGGER: Command Trigger; partner
+USAGE METHOD: -partner <accept|deny|request|mention> <if request is used: mention>
+*/}}
